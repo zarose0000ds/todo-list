8b586ae4c8921e840365b4b519c7a17d209069c7
zarose0000ds <zarose0000ds@gmail.com>
2021-09-25 23:24:19 +0800

    feat: add habdlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
@@ -14,9 +15,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// TEMPLATE ENGINE
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
 // LISTENING
