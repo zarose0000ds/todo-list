9cde3cf4fa597c202f43ecb9ae1712d1d9356d64
zarose0000ds <zarose0000ds@gmail.com>
2021-09-25 23:07:58 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// SETTING DATABASE CONNECTION
+mongoose.connect('mongodb://localhost/todo-list')
+const db= mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // SETTING ROUTES
 app.get('/', (req, res) => {
